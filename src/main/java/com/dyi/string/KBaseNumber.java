75af48a16995ad1dbfbf27a30259a8b5e43b00a2
stagebo <1254117589@qq.com>
2017-12-21 09:11:21 +0800

    十进制转码。2017-12-21 9:11:19

---
+++ src/main/java/com/dyi/string/KBaseNumber.java
@@ -0,0 +1,44 @@
+package com.dyi.string;
+
+public class KBaseNumber {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			System.out.print("" + i + "__十进制: "
+					+ parseKBase(i, "123456789".split("")));
+			System.out.print("," + "二进制: " + parseKBase(i, "1".split("")));
+			System.out.print("," + "八进制: "
+					+ parseKBase(i, "1234567".split("")));
+			System.out.print("," + "16进制: "
+					+ parseKBase(i, "123456789ABCDEF".split("")));
+			System.out.println("," + "A-Z进制: " + parseKBase(i));
+		}
+	}
+
+	public static String parseKBase(int num) {
+		String[] base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+		return parseKBase(num, base);
+	}
+
+	public static String parseKBase(int num, String[] base) {
+		if (num == 0) {
+			return "0";
+		}
+
+		int k = base.length;
+		String sign = num >= 0 ? "" : "-";
+		num = num >= 0 ? num : -num;
+		StringBuilder result = new StringBuilder();
+
+		while (num > 0) {
+			int idx = num % k;
+			num = num / k;
+			if (idx == 0) {
+				result.append("0");
+			} else {
+				result.append(base[idx]);
+			}
+		}
+		result.append(sign);
+		return result.reverse().toString();
+	}
+}
